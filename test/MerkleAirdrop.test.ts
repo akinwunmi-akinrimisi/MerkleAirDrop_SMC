9e835c8dfd9b9ab3377fc93fc6b5afd6be14eaa2
akinwunmi-akinrimisi <akinolaakinrimisi@gmail.com>
2024-09-18 19:01:00 +0100

    modifications to the script

--- test/MerkleAirdrop.test.ts
+++ test/MerkleAirdrop.test.ts
@@ -6,7 +6,6 @@ async function main() {
     const BAYC_CONTRACT_ADDRESS = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"; // BAYC contract on Ethereum mainnet
     const ERC20_TOKEN_ADDRESS = "0xb55d31e46428bEB28234b62D5178DAd5Ec8aD8ca"; //  ERC20 token address
     const BAYC_HOLDER = "0x76C1cFe708ED1d2FF2073490727f3301117767e9"; // address of a BAYC holder
-    const AirdropContractAddress = "0xYourAirdropContractAddress"; 
 
     await helpers.impersonateAccount(BAYC_HOLDER);
     const impersonatedSigner = await ethers.getSigner(BAYC_HOLDER);
